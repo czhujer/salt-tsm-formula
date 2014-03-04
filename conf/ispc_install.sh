8a81bec0d89843786712dc20dbeda59eb0d07020
Ales Komarek <mail@newt.cz>
2014-03-04 12:03:36 +0100

    cleanup file

--- conf/ispc_install.sh
+++ conf/ispc_install.sh
@@ -1,3 +1,58 @@
+#/bin/sh
+#
+# ispc_inst.sh <NODE_NAME> <PASSWORD>
+#
+# T.Jindra, 7.2013, AG COM a.s.
+
+# BEGIN EDITABLE GLOBAL VARIABLES
+
+
+# BEGIN SCRIPT VARIABLES
+if [ -e /etc/redhat-release ]
+ then
+  OSLEVEL=`cat /etc/redhat-release`
+  INITDEFAULT=`cat /etc/inittab | grep initdefault | grep -v ^# | cut -d : -f 2`
+fi
+if [ -e /usr/bin/lsb_release ]
+ then
+  OSLEVEL=`/usr/bin/lsb_release -d | sed -e 's/Description:[\t]//g' | cut -b 1-12`
+  INITDEFAULT=`cat /etc/init/rc-sysinit.conf | grep "env DEFAULT_RUNLEVEL" | cut -d "=" -f 2`
+fi
+DSMSYS=dsm_gen.sys
+COMMON_INSTALL=com64
+ARCH_X64=`uname -a | grep -c x86_64`
+CS_LOCALE=`env | grep LANG | grep -c cs`
+WORK_DIR=`pwd`
+
+# BEGIN GLOBAL FUNCTIONS
+# Install TSMC on Centos 6.x
+InstCentOS6()
+{
+if [ $ARCH_X64 -eq 1 ]
+ then
+   yum localinstall -y ./CentOS_6.5/* ./com64/*
+   if [ $CS_LOCALE -eq 1 ]
+    then
+     yum localinstall -y ./com64/CSY/*
+   fi
+ else
+   yum localinstall -y ./Linux86/*
+   if [ $CS_LOCALE -eq 1 ]
+    then
+     yum localinstall -y ./Linux86/CSY/*
+   fi
+fi
+}
+
+# Install TSMC on Ubuntu 12
+InstUbnt12()
+{
+/usr/bin/aptitude update
+/usr/bin/aptitude full-upgrade
+/usr/bin/aptitude install alien libstdc++6 ksh ia32-libs
+/usr/bin/dpkg -i ./Ubnt_12.4_x64/gskcrypt64-8.0.deb
+/usr/bin/dpkg -i ./Ubnt_12.4_x64/gskssl64-8.0.deb
+/usr/bin/dpkg -i ./Ubnt_12.4_x64/TIVsm-API64-6.3.1.deb
 /usr/bin/dpkg -i ./Ubnt_12.4_x64/TIVsm-BA-6.3.1.deb
 mkdir /var/lock/subsys
 echo "/opt/tivoli/tsm/client/api/bin64" > /etc/ld.so.conf.d/tivoli.conf
@@ -45,6 +100,9 @@ NODE_NAME=$1
 
 case $OSLEVEL in
  "CentOS release 6.5 (Final)") OS_DEP_INSTALL=CentOS_6.5
+
+ ;;
+ "CentOS release 6.4 (Final)") OS_DEP_INSTALL=CentOS_6.4
 
  ;;
  "CentOS release 6.3 (Final)") OS_DEP_INSTALL=CentOS_6.3
